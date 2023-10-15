30e3c15d1b48e69cdaa3641f53aeead74842ccb1
3morprog <128446667+3morprog@users.noreply.github.com>
2023-10-15 09:44:55 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{ 0;
+    int x,y, sum ;
+    cout <<"enter two numbers "<<endl;
+    cin>>x>>y;
+    sum=x+y;
+    cout<<"sum ="<<sum;
+}
